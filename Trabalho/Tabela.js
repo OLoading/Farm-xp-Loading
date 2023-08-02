700100f02d6cbdf876493d2cbf0a53fee5650624
OLoading <luander2010@outlook.com>
2023-08-02 20:43:01 -0300

    .

--- Trabalho/Tabela.js
+++ Trabalho/Tabela.js
@@ -1,14 +1,20 @@
-console.log("1-menu")
-console.log("2-cadastro de aluno")
-console.log("3-listar todos os alunos")
-console.log("4-listar aluno em específico")
-console.log("5-atualizar aluno")
-console.log("6-excluir aluno")
-console.log("7-media")
-console.log("8-main")
-console.log(" ")
-console.log("Id:")
-console.log("Nome:")
-console.log("Matricula:")
-console.log("Idade:")
-console.log("nota:")
+const egoist = require("readline-sync").question
+
+function menu(){
+    while(1){
+        console.log("--menu--")
+        console.log("1 - cadastro de aluno")
+        console.log("2 - listar todos os alunos")
+        console.log("3 - listar aluno em específico")
+        console.log("4 - atualizar aluno")
+        console.log("5 - excluir aluno")
+        console.log("8 - media")
+        console.log("9 - main")
+        console.log("0 - Sair")
+        break  
+    }
+    
+    var a = egoist("Digite uma opcao: ")
+}
+
+console.log(menu())
---
+++ Trabalho/functionswitch.js
@@ -0,0 +1,58 @@
+function obteropcao (){
+            return paerseInt(prompt("Digite o numero da opcao desejada: "))
+        }
+    
+    function executaropcao(opcao){
+        switch (opcao) {
+            case 1:
+                console.log("Você escolheu a Opção 1.")
+                break
+            
+            case 2:
+                console.log("Você escolheu a Opção 2.")
+                break
+                
+            case 3:
+                console.log("Você escolheu a Poção 3.")
+                break
+            
+            case 4:
+                console.log("Você escolheu a Opção 4.")
+                break
+    
+            case 5:
+                console.log("Você escolheu a Opção 5.")
+                break
+    
+            case 6:
+                console.log("Você escolheu a Opção 6.")
+                break
+    
+            case 7:
+                console.log("Você escolheu a Opção 6.")
+                break
+    
+            case 8:
+                console.log("Você escolheu a Opção 7.")
+                break
+    
+            case 9:
+                console.log("Você escolheu a Opção 8.")
+                break
+            
+            default:
+                console.log("Concluido.")
+            }
+    }
+    
+    function main(){
+        let opcao
+    
+    do {
+        exibirMenu()
+        opcao = obteropcao()
+        executaropcao(opcao)
+     } while (opcao !== 4)
+    }
+
+main()
